da9b4e8bb84642f3f805b7a95f26f0fb52400d43
George Maddocks <george.maddocks@krakenflex.com>
2024-11-28 16:47:58 +0000

    Day 16 part one refactor

--- day_16/main.py
+++ day_16/main.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from utils import open_file
 
 
@@ -14,23 +16,23 @@ class BeamState:
         self.y = y
         self.location = f"{x},{y}"
         self.direction = direction
-        self.location_direction_combo = f"{x},{y},{direction}"
+        self.loc_dir_combo = f"{self.location},{direction}"
 
 
 def __solution_1(grid: list[str]):
     x_max = len(grid[0]) - 1
     y_max = len(grid) - 1
     tiles_energised = set[str]()
-    previous_location_direction_combos = set[str]()
+    previous_loc_dir_combos = set[str]()
     queue: list[BeamState] = [BeamState(x=0, y=0, direction="R")]
     while len(queue):
         beam_state = queue.pop(0)
         tiles_energised.add(beam_state.location)
-        previous_location_direction_combos.add(beam_state.location_direction_combo)
+        previous_loc_dir_combos.add(beam_state.loc_dir_combo)
         queue.extend(
             _get_new_beam_states(
                 beam_state,
-                previous_location_direction_combos,
+                previous_loc_dir_combos,
                 x_max,
                 y_max,
                 current_grid_value=grid[beam_state.y][beam_state.x],
@@ -42,37 +44,30 @@ def __solution_1(grid: list[str]):
 
 def _get_new_beam_states(
     current_beam_state: BeamState,
-    previous_location_direction_combos: set[str],
+    previous_loc_dir_combos: set[str],
     x_max: int,
     y_max: int,
     current_grid_value: str,
 ) -> list[BeamState]:
-    new_directions = (
-        [current_beam_state.direction]
-        if current_grid_value == "."
-        else _direction_mirror_lookup[current_beam_state.direction][current_grid_value]
-    )
-    new_coords = [
-        direction_to_location(direction, (current_beam_state.y, current_beam_state.x))
-        for direction in new_directions
-    ]
-    new_beam_states = [
-        BeamState(
-            x=new_coords[idx][1],
-            y=new_coords[idx][0],
-            direction=new_directions[idx],
+    def _maybe_create_new_beam_state(
+        acc: list[BeamState], direction: str
+    ) -> list[BeamState]:
+        (x, y) = direction_to_location(
+            direction, (current_beam_state.x, current_beam_state.y)
         )
-        for idx in range(0, len(new_coords))
-    ]
-    return [
-        new_beam_state
-        for new_beam_state in new_beam_states
+        new_beam_state = BeamState(x, y, direction)
         if not (
-            _beam_has_left_grid((new_beam_state.y, new_beam_state.x), x_max, y_max)
-            or new_beam_state.location_direction_combo
-            in previous_location_direction_combos
-        )
-    ]
+            _beam_has_left_grid((new_beam_state.x, new_beam_state.y), x_max, y_max)
+            or new_beam_state.loc_dir_combo in previous_loc_dir_combos
+        ):
+            acc.append(new_beam_state)
+        return acc
+
+    return reduce(
+        _maybe_create_new_beam_state,
+        _direction_mirror_lookup[current_beam_state.direction][current_grid_value],
+        [],
+    )
 
 
 _direction_mirror_lookup = {
@@ -81,24 +76,28 @@ _direction_mirror_lookup = {
         "\\": ["D"],
         "|": ["U", "D"],
         "-": ["R"],
+        ".": ["R"],
     },
     "L": {
         "/": ["D"],
         "\\": ["U"],
         "|": ["U", "D"],
         "-": ["L"],
+        ".": ["L"],
     },
     "U": {
         "/": ["R"],
         "\\": ["L"],
         "|": ["U"],
         "-": ["L", "R"],
+        ".": ["U"],
     },
     "D": {
         "/": ["L"],
         "\\": ["R"],
         "|": ["D"],
         "-": ["L", "R"],
+        ".": ["D"],
     },
 }
 
@@ -106,22 +105,22 @@ _direction_mirror_lookup = {
 def direction_to_location(
     direction: str, current_coords: tuple[int, ...]
 ) -> tuple[int, ...]:
-    (y, x) = current_coords
+    (x, y) = current_coords
     match direction:
         case "R":
-            return (y, x + 1)
+            return (x + 1, y)
         case "L":
-            return (y, x - 1)
+            return (x - 1, y)
         case "U":
-            return (y - 1, x)
+            return (x, y - 1)
         case "D":
-            return (y + 1, x)
+            return (x, y + 1)
         case _:
             raise ValueError(f"Invalid direction: {direction}")
 
 
 def _beam_has_left_grid(new_coords: tuple[int, ...], x_max: int, y_max: int) -> bool:
-    (y, x) = new_coords
+    (x, y) = new_coords
     return x < 0 or y < 0 or x > x_max or y > y_max
 
 
